a2cc862fb9be2d481fd4dea7d0f75fe68f122b6a
siva-ranjani-r <siva2300rs@gmail.com>
2023-01-18 17:43:18 +0530

    updated.

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,url_for,redirect,request
+import sqlite3 as sql
+
+app=Flask("__name__")
+
+
+@app.route("/",methods=["POST","GET"])
+def fun():
+    if request.form.get("username")!=None:
+        Username=request.form.get("Username")
+        Mobile_number=request.form.get("Mobile_number")
+        password=request.form.get("password")
+
+        conn=sql.connect("data.db")
+        cur=conn.cursor()
+
+        cur.execute("select username from data1")
+        data=cur.fetchall()
+
+        l=[]
+        for i in data:
+            l.append(i[0])
+
+        for i in l:
+            if i==Username:
+                return redirect(url_for("signUp"))
+
+        cur.execute("insert into data1 (Username,Mobile_number,password) values (?,?,?)",(Username,Mobile_number,password))
+        conn.commit()
+
+    return render_template("login.html")
+
+@app.route("/signUp")
+def signUp():
+    return render_template("signUp.html")
+
+@app.route("/home")
+def fun1():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,16 @@
+var Selected_Seats=0,price=0;
+var squareRef = document.getElementsByClassName("row");
+for(var i = 0; i < squareRef.length; i++) {
+    squareRef[i].addEventListener("click", changeColor);
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "#6feaf6";
+    location.replace("")
+    Selected_Seats+=1;
+    price+=850
+    x=document.getElementById("count")
+    x.innerHTML=Selected_Seats
+    y=document.getElementById("total")
+    y.innerHTML=price
+}
